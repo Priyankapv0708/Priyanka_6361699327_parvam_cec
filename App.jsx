40adfd79f6d2d72873652e5411c1f1d75a8254fb
Priyankapv0708 <priyankapv0708@gmail.com>
2025-10-18 13:18:06 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,56 +1,82 @@
-import React from "react";
-import { Greeting } from "./components/Propert";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Experience } from "./components/Experience";
+import { Contact } from "./components/Contact";
 
-export const Objects = () => {
-  const students = [
-    { id: 1,name:"John", age: 22, course: "React"},
-    { id: 2,name:"Dustin", age: 21, course: "Node.js"},
-    { id: 3,name:"Steven",age: 24,course: "JavaScript"},
-  ];
-  // const students = ["Jhon","Mary","Alex","Sara","Sujan"];
-  // const [isloggedIn, setIsLoggedIn] = useState(false);
-  return (
-    <>
-      {/* <dev>
-        <h2>Conditional rendering...</h2>
-        {isloggedIn ? (
-          <h3>Welcome back , user</h3>
-        ) : (
-          <h3>Please log in to continue..</h3>
-        )}
 
-        <button onClick={() => setIsLoggedIn(!isloggedIn)}>
-          {isloggedIn ? "LogOut" : "Login"}
-        </button>
-      </dev> */}
-      
-{/*
-        <div>
-            <h4>List rendering...</h4>
-            <ul>
-                {students.map((name,index) => (
-                  <li key={index}>
-                    {index + 1}.{name}
-                  </li>
-                ))}
-              </ul>
-            </div> */}
 
-        <div>
-          <Greeting name="Priya" age="19"></Greeting>
+function App() {
+    return (
+        <>
+        <div className='bg-dark min-vh-100 d-flex  p-3 overflow-hidden'>
+            <div className="bg-black  flex-grow-1 rounded-3">
+                <br></br>
+              <div className="row text-light  ">
+                <div className="col-1"></div>
+                <div className="col-5 ">
+                    <p class="fst-italic fs-1">solo</p>
+                </div>
+                <div className="col-5 bg-dark p-2 rounded-5">
+                    <ul className="nav row ">
+                        <a className="col-1"></a>
+                        <li className="col-2">
+                            <Link to="/" className="me-2">About</Link>
+                        </li>
+                        <li className="col-2">
+                           <Link to="/" className="me-2">Skills</Link>
+                           </li>
+                        <li className="col-2">
+                            <Link to="/projects" className="me-2">Projects</Link>
+                        </li>
+                        <li className="col-2">
+                            <Link to="/experience" className="me-2" >Experience</Link>
+                        </li>
+                        <li className="col-2">
+                           <Link to="/" className="me-2">Contact</Link>
+                        </li>
+                        <a className="col-1"></a>
+                        </ul>
+                                
+                </div>
+                <div className="col-1"></div>
+              </div>
+              <br></br>
+              <br></br>
+
+                <div className="row ">
+                    <div className="col-1"></div>
+                     <div className="col-10 ">
+                        <div className="text-light">
+                     <Routes>
+                    <Route path="/" element={<About />}></Route>
+                    <Route path="/experience" elements={<Experience />}></Route>
+                    <Route path="/projects" element={<Projects />}></Route>
+                    <Route path="/skills" elements={<Skills />}></Route>
+                    <Route path="/contact" elements={<Contact />}></Route>
+                    
+                    </Routes>
+                </div>
+                     </div>
+                     <div className="col-1"></div>
+                    
+                </div>
+                
+              
+
+                
+               
+               
+            </div>
         </div>
-        <div>
-            {students.map((student) => (
-              <p>
-                Name:{student.name} | Age:{student.age} | Course:{student.course}
-              </p>
-            ))}
-          
-        </div>
-      </>
-  );
-};
+        
+        
+       
+        </>
+    )
+}
 
-
-
-export default Objects
+export default App;
